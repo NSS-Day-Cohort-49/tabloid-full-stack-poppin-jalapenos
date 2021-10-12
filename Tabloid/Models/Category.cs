ab0a0386adc2001c06aee69aa7c6bf8e86d33b24
Joel Beckum <joelbeckum@gmail.com>
2021-10-12 13:55:48 -0500

    Added Post model, repo, and controller to API app

---
+++ Tabloid/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Tabloid.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+            
+        [Required]
+        public string Name { get; set; }
+    }
+}
--- Tabloid/Startup.cs
+++ Tabloid/Startup.cs
@@ -22,7 +22,9 @@ namespace Tabloid
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
             services.AddTransient<IUserProfileRepository, UserProfileRepository>();
+            services.AddTransient<IPostRepository, PostRepository>();
 
             var firebaseProjectId = Configuration.GetValue<string>("FirebaseProjectId");
             var googleTokenUrl = $"https://securetoken.google.com/{firebaseProjectId}";
